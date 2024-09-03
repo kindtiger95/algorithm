484cb49f698c31143282e53646071e164a3e57c5
janginbeom <jjanginbeom@naver.com>
2024-09-03 10:14:57 +0900

    add: part2

--- thisiscodingtest/src/part2/Part2.kt
+++ thisiscodingtest/src/part2/Part2.kt
@@ -1,4 +1,45 @@
 package part2
 
 class Part2 {
+    companion object {
+        // 실전문제 2번, p.92
+        fun practicalProblem2(m: Int, k: Int, numberArray: List<Int>): Int {
+            val sortedNumberList = numberArray.sortedDescending()
+            val multiply = m / (k + 1)
+            val multiplyResult = (sortedNumberList[0] * k + sortedNumberList[1]) * multiply
+            val remain = m % (k + 1)
+            val remainResult = remain * sortedNumberList[0]
+            return multiplyResult + remainResult
+        }
+
+        // 실전문제 3번, p.96
+        fun practicalProblem3(numbers: List<List<Int>>): Int {
+            var result = -1
+            numbers.forEach {
+                val min = it.min()
+                result = if (result < 0) min else if (result > min) result else min
+            }
+            return result
+        }
+
+        // 실전문제 4번, p.99
+        fun practicalProblem4(n: Int, k: Int): Int {
+            var result = 0
+            var target = n
+            while (target != 1) {
+                val remain = target % k
+                if (remain == 0) {
+                    target /= k
+                    result++
+                } else if (remain < target) {
+                    target -= remain
+                    result += remain
+                } else {
+                    target -= 1
+                    result += 1
+                }
+            }
+            return result
+        }
+    }
 }
